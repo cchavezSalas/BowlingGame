edc48d106caa708674168e4b1be12f38f7dbb81e
cchavez <chavez_christian1409@yahoo.es>
2022-12-05 10:49:32 -0500

    File Validation

--- Logic/FileManagement/SourceManager.cs
+++ Logic/FileManagement/SourceManager.cs
@@ -1,14 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Logic.FileManagement
 {
     public class SourceManager
     {
+        private string _filePath;
+        private readonly string _validExtension = ".txt";
 
+        public SourceManager(string filePath)
+        {
+            this._filePath = filePath;
+        }
+
+
+
+        public bool IsValidFile()
+        {
+
+            if (!File.Exists(this._filePath))
+            {
+                return false;
+            }
+
+            var fileExt = Path.GetExtension(this._filePath);
+            if (fileExt != this._validExtension)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+
+
+
+        public List<Chance> GetChances()
+        {
+
+            throw new NotImplementedException();
+            
+
+
+
+
+
+        }
+
+
+        private Chance ConvertLine(string line)
+        {
+
+            throw new NotImplementedException();
         
-        
+        }
+
 
 
 
---
+++ Test/FileTester.cs
@@ -0,0 +1,84 @@
+﻿using Logic.FileManagement;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Test
+{
+    public class FileTester
+    {
+
+        private  string _validFileCreated;
+        private  string _notValidFileCreated;
+
+        [SetUp]
+        public void SetUpFileTests()
+        {
+
+            this._validFileCreated = "validFileDummy.txt";
+            CreateFile(this._validFileCreated);
+
+            /*
+             The second one has a wrong extension
+             */
+
+            this._notValidFileCreated = "notValidDummy.log";
+            CreateFile(this._notValidFileCreated);
+            
+        
+        }
+
+
+        private void CreateFile(string fileName)
+        {
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            string[] lines =
+                {
+                "Chris 10", "Chris 7", "John F"
+                };
+            File.WriteAllLines(fileName,lines);
+        }
+
+
+
+
+
+        [Test]
+        public void FileNotFound()
+        {
+            string filePathNotExisting = @"D:\newdirectory\filenotexists.txt";
+            SourceManager sm = new SourceManager(filePathNotExisting);
+            bool isFileValid = sm.IsValidFile();
+            Assert.IsFalse(isFileValid);
+        }
+
+
+        [Test]
+        public void NotValidFormat()
+        {
+            string filePathNotExisting = this._notValidFileCreated;
+            SourceManager sm = new SourceManager(filePathNotExisting);
+            bool isFileValid = sm.IsValidFile();
+            Assert.IsFalse(isFileValid);
+
+        }
+
+        [Test]
+        public void ValidFileFormat()
+        {
+            string filePath = this._validFileCreated;
+            SourceManager sm = new SourceManager(filePath);
+            bool isFileValid = sm.IsValidFile();
+            Assert.IsTrue(isFileValid);
+        }
+
+
+    }
+}
